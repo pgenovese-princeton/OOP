d580bcce968d0e4476395bd0d85ad4503b34be5e
Phil Genovese <pgenovese@princeton.com>
2023-03-21 12:29:43 -0400

    Car and Truck extend Vehicle

---
+++ src/main/java/com/example/oop/Truck.java
@@ -0,0 +1,10 @@
+package com.example.oop;
+
+public class Truck extends Vehicle {
+
+    private int numWheels;
+
+    public Truck(int width, int length) {
+        super(width, length, true);
+    }
+}
