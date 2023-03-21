73d3ea0df064766ea8200e7c3b5594843831b338
Phil Genovese <pgenovese@princeton.com>
2023-03-21 12:30:24 -0400

    Bicycle that has no engine but uses composition to still inherit from Vehicle

---
+++ src/main/java/com/example/oop/Bicycle.java
@@ -0,0 +1,10 @@
+package com.example.oop;
+
+public class Bicycle extends Vehicle {
+
+    private boolean hasCupHolder;
+
+    public Bicycle(int width, int length) {
+        super(width, length, false);
+    }
+}
