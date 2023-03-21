a4ffefb1334b8fe953c51f6565815c48e6a2f67f
Phil Genovese <pgenovese@princeton.com>
2023-03-21 12:31:08 -0400

    example with List of Vehicles

--- src/main/java/com/example/oop/Driver.java
+++ src/main/java/com/example/oop/Driver.java
@@ -1,12 +1,24 @@
 package com.example.oop;
 
 
+import com.intellij.util.containers.SortedList;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
 public class Driver {
     public static void main(String args[]){
 
         Car workCar = new Car(4, 8);
 
         Car personalCar = new Car(2, 6);
+
+        Truck workTruck = new Truck(8, 16);
+
+        // this Arrays.asList is a ~static function~, consider it magic for creating a List, for now
+        List<Vehicle> listOfMyCars = Arrays.asList(workCar, personalCar, workTruck);
 
         boolean atWork = false;
 
